3ab43f29983953b028f2c6806ebe5f14d42d0462
Nikolaykrasnov <crasnov.colya2000@yandex.ru>
2023-02-23 16:33:32 +0500

    Добавьте файлы проекта.

---
+++ Operator SWICH.cpp
@@ -0,0 +1,52 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int day;
+	printf("Day?");
+	scanf_s("%i", &day);
+
+	switch (day)
+	{
+	case 1:
+	{
+		printf("Tue");
+	}
+	break;
+	case 2:
+	{
+		printf("Mon");
+	}
+	break;
+	case 3:
+	{
+		printf("Wed");
+	}
+	break;
+	case 4:
+	{
+		printf("Thu");
+	}
+	break;
+	case 5:
+	{
+		printf("Fri");
+	}
+	break;
+	case 6:
+	{
+		printf("Sat");
+	}
+	break;
+	case 7:
+	{
+		printf("Sun");
+	}
+	break;
+	default:
+	{
+		printf("Error");
+	}
+		
+	}
+}
